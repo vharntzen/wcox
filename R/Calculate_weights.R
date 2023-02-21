b6a354d7fd6f48e03ea3bb31549583ab85a25b2b
vharntzen <v.h.arntzen@math.leidenuniv.nl>
2023-02-21 10:33:56 +0100

    Improve lay-out of code and clarity of the explanation in the code.

--- R/Calculate_weights.R
+++ R/Calculate_weights.R
@@ -7,18 +7,18 @@
 #' cancer research.
 #'
 #' @details
-#' Weights are based on a comparison between the survival between sample and population.
-#' Therefore, besides the sample data, the population incidence rate (per 100 000) is needed as
-#' input, as well as the cut-offs of the (age/time-to-event) groups for which
-#' this is available. The function provides two options for the latter:
-#' cut-offs can be provided manually or using the standard 5- or 10-years (age)
-#' categories (0-4, 5-9, ... or 0-9, 10-14, ...). Note that resulting intervals
-#' are of the form [xx, xx).
+#' Weights are based on a comparison between the survival between sample and
+#' population. Therefore, besides the sample data, the population incidence rate
+#' (per 100 000) is needed as input, as well as the cut-offs of the
+#' (age/time-to-event) groups for which this is available. The function provides
+#' two options for the latter: cut-offs can be provided manually or using the
+#' standard 5- or 10-years (age) categories (0-4, 5-9, ... or 0-9, 10-14, ...).
+#' Note that resulting intervals are of the form [xx, xx).
 #'
 #' @export
 
 
-Calculate_weights <- function(dat) {
+Calculate_weights <- function(dat){
 
 ### Input:
 
@@ -33,37 +33,58 @@ Calculate_weights <- function(dat) {
 
 #' @return Vector with weights.
 
-# Extract variables from input data.frame:
-  k <- dat$k # Group/interval
-  S_k <- dat$S_k # Population proportion of individuals experiencing the event in intervals later than k
-  S_k. <- dat$S_k. # Sample proportion of individuals experiencing the event in intervals later than k
+  # --------------- Extract variables from input data.frame.
 
-# Create empty containers:
+  # Group/interval.
+  k <- dat$k
+
+  # Population proportion of individuals experiencing the event in intervals
+  # later than k.
+  S_k <- dat$S_k
+
+  # Sample proportion of individuals experiencing the event in intervals
+  # later than k.
+  S_k. <- dat$S_k.
+
+  # --------------- Create empty containers.
+
   v <- w <- rep(NA, nrow(dat))
 
-# Calculate the weights:
+  # --------------- Calculate the weights.
+
   for (n in 1:nrow(dat)){
-    v[n] = 1  # Weights for unaffected
-    w[n] = ( (1 - S_k[n]) / S_k[n] ) * (S_k.[n] / ( 1 - S_k.[n]))  # Weights for cases
+
+    # Weights for unaffected.
+    v[n] = 1
+
+    # Weights for cases.
+    w[n] = ( (1 - S_k[n]) / S_k[n] ) * (S_k.[n] / ( 1 - S_k.[n]))
   }
 
   merged <- cbind(dat, w, v)
   merged$weight <- NA
 
-# Assign w for uncensored, v for censored (interval-wise):
+  # --------------- Assign w for uncensored, v for censored (interval-wise).
+
   merged$weight[which(merged$d == 1)] <- merged$w[which(merged$d == 1)]
   merged$weight[which(merged$d == 0)] <- merged$v[which(merged$d == 0)]
 
-  merged$w <- merged$v <- NULL  # Remove old variable
+  merged$w <- merged$v <- NULL  # Remove old variable.
 
-# Collect output:
+  # --------------- Collect output.
+
   vec_weights <- merged$weight
-  vec_weights[which(vec_weights==0)] <- 0.0000001  # If weight is 0, add very small value (coxph does not accept weights of 0).
 
-# Print warning if weights are invalid:
-  ifelse((sum(vec_weights<0)>0), print("Invalid (negative) weights!"), print("No negative weights"))  # If there are weights of zero, skip this iteration
+  # If weight is 0, add very small value (coxph does not accept weights of 0).
+  vec_weights[which(vec_weights==0)] <- 0.0000001
 
-# Return output:
+  # --------------- Print warning if weights are invalid.
+
+  ifelse((sum(vec_weights<0)>0), print("Invalid (negative) weights!"),
+         print("No negative weights"))
+
+  # --------------- Return output.
+
   vec_weights
 
 }
--- R/Prepare_data.R
+++ R/Prepare_data.R
@@ -20,9 +20,10 @@ Prepare_data <- function(dat, population_incidence, breaks){
 #' a column **d** with event indicator (1 for event, 0 for censored), a column
 #' **y** with event/censoring time.
 #' @param population_incidence A vector (in combination with breaks) or
-#' a data.frame (columns 1) 'start age group', 2) 'end age group', 3)'S_pop') with
-#' population incidence per 100,000 per interval k.
-#' @param breaks Cut-points for the (age/time) groups. Only needed when population_incidence is a vector.
+#' a data.frame (columns 1) 'start age group', 2) 'end age group', 3)'S_pop')
+#' with population incidence per 100,000 per interval k.
+#' @param breaks Cut-points for the (age/time) groups. Only needed when
+#' population_incidence is a vector.
 
   ### Output:
 
@@ -33,35 +34,48 @@ Prepare_data <- function(dat, population_incidence, breaks){
   #' proportion of individuals experiencing the event in intervals later
   #' than k.
 
+  # --------------- Load packages.
+
   require(survival)
   require(dplyr)
   require(tidyr)
 
-  ### From population incidence rate to S_k:
+  # --------------- Organize external information (population incidence rate).
+  # N.B.: there are different options regarding the breaks (1-4).
 
-  # Organize external information (population incidence rate)
   if(is.vector(population_incidence)){
+
   if(is.numeric(breaks)){ # Option 1: two vectors population_incidence and breaks.
-    if(length(breaks)!=(length(population_incidence)+1) ) warning("Number of breaks should equal the number of groups plus one.")
+    if(length(breaks)!=(length(population_incidence)+1) ) warning("Number of
+                                                                  breaks should
+                                                                  equal the
+                                                                  number of
+                                                                  groups plus
+                                                                  one.")
     n_agegroups <- length(population_incidence)
     from <- breaks[-(n_agegroups + 1)]  # Remove last
     to <- breaks[-1] # Remove first
-    dat_inc <- data.frame(start = from, end = to, incidence_rate = population_incidence/100000)
-  } else if(breaks == "5 years"){ # Option 2: 5 years age groups.
+    dat_inc <- data.frame(start = from, end = to, incidence_rate =
+                            population_incidence/100000)
+
+    } else if(breaks == "5 years"){ # Option 2: 5 years age groups.
     n_agegroups <- length(population_incidence)
     breaks <- seq(0, 5*(n_agegroups), by = 5)
     from <- breaks[-(n_agegroups + 1)]  # Remove last
     to <- breaks[-1] # Remove first
-    dat_inc <- data.frame(start = from, end = to, incidence_rate = population_incidence/100000)
-  } else if(breaks == "10 years"){ # Option 3: 10 years age groups.
+    dat_inc <- data.frame(start = from, end = to, incidence_rate =
+                            population_incidence/100000)
+
+    } else if(breaks == "10 years"){ # Option 3: 10 years age groups.
     n_agegroups <- length(population_incidence)
     breaks <- seq(0, 10*(n_agegroups), by = 10)
     from <- breaks[-(n_agegroups + 1)]  # Remove last
     to <- breaks[-1] # Remove first
-    dat_inc <- data.frame(start = from, end = to, incidence_rate = population_incidence/100000)
+    dat_inc <- data.frame(start = from, end = to, incidence_rate =
+                            population_incidence/100000)
   }
-  } else { # If population incidence is given as a data.frame:
 
+  } else { # Option 4: if population incidence is given as a data.frame.
     dat_inc <- data.frame(start = population_incidence[,1],
                           end = population_incidence[,2],
                           incidence_rate = population_incidence[,3]/100000)
@@ -70,58 +84,87 @@ Prepare_data <- function(dat, population_incidence, breaks){
 
   dat_inc <- dat_inc %>% mutate( k = paste("(", start, ",", end, "]", sep=""),
                                  t = end - start)
+                                 # t is the width of the age interval, in years.
+                                 # Note that in the paper, this is referred to
+                                 # as (alpha_k - alpha_({k-1}).
 
-  # Calculate S_k population from population incidence.
-  Surv_k <- exp(-cumsum(dat_inc$t * dat_inc$incidence_rate)) # Survival up to end of interval.
+  # --------------- Calculate S_k population from population incidence.
+
+  Surv_k <- exp(-cumsum(dat_inc$t * dat_inc$incidence_rate))
+  # Surv_k = survival up to end of interval.
+
+  # Determine S_k for each age group k.
   S_k <- c() # Initialize vector.
-  S_k[1] <- 1 # NEW
+
+  S_k[1] <- 1
   for (i in 2:n_agegroups){
     S_k[i] <- Surv_k[i]/Surv_k[i-1]
   }
+
   dat_inc$S_k <- S_k
 
-  # --------------- Inputs for weights. S_k. based on sample data:
+  # --------------- Calculate S_k. based on sample data.
 
   dat$y_cat <- cut(dat$y, breaks = breaks)
   index <- which(is.na(dat$y_cat)==T)
   if(length(index)>0){
     dat<- dat[-index,]}  # Include only those that fall within an age category.
 
-  # Store number of cases and unaffected individuals per age group
+  # Store number of cases and unaffected individuals per age group.
   agegroups.info <- aggregate(id ~ y_cat + d, data = dat, length)
 
   agegroups.info <- agegroups.info %>% spread(d, -y_cat)
   colnames(agegroups.info) <- c("k","s_k","r_k")
 
-  # Transform NA to 0 (its real meaning)
+  # Transform NA to 0 (its real meaning).
   agegroups.info$s_k <- ifelse(!is.na(agegroups.info$s_k), agegroups.info$s_k, 0)
   agegroups.info$r_k <- ifelse(!is.na(agegroups.info$r_k), agegroups.info$r_k, 0)
 
-  # Calculate person years per outcome and age group
-  personyears <- aggregate(y ~ y_cat + d, data = dat[,c("d","y_cat", "y"),], sum) %>% spread(d, -y_cat)
+  # Calculate person years per outcome and age group.
+  personyears <- aggregate(y ~ y_cat + d, data =
+                             dat[,c("d","y_cat", "y"),], sum) %>%
+                             spread(d, -y_cat)
   colnames(personyears) <- c("k","q_k","p_k")
   agegroups.info <- merge(agegroups.info, personyears, by = "k")  # Merge.
 
-  agegroups.info$q_k <- ifelse(!is.na(agegroups.info$q_k), agegroups.info$q_k, 0)
-  agegroups.info$p_k <- ifelse(!is.na(agegroups.info$p_k), agegroups.info$p_k, 0)
+  agegroups.info$q_k <- ifelse(!is.na(agegroups.info$q_k),
+                               agegroups.info$q_k, 0)
+  agegroups.info$p_k <- ifelse(!is.na(agegroups.info$p_k),
+                               agegroups.info$p_k, 0)
 
   agegroups.info$years.before <- breaks[which(breaks != max(breaks))]
-  q <- agegroups.info$q_k - (agegroups.info$years.before * agegroups.info$s_k)  # Person years among censored individuals.
-  p <- agegroups.info$p_k - (agegroups.info$years.before * agegroups.info$r_k)  # Person years among cases.
-  t <- agegroups.info$t <- dat_inc$t  # Years per age group
-  s <- agegroups.info$s_k  # Number of censored.
-  r <- agegroups.info$r_k  # Number of cases.
-  k <- agegroups.info$k  # Age group label.
 
-  # Incidence rate based on sample data:
+  # Person years among censored individuals.
+  q <- agegroups.info$q_k - (agegroups.info$years.before * agegroups.info$s_k)
+
+  # Person years among cases.
+  p <- agegroups.info$p_k - (agegroups.info$years.before * agegroups.info$r_k)
+
+  # Years per age group.
+  # N.B. in the paper referred to as (alpha_k - alpha_{k-1}).
+  t <- agegroups.info$t <- dat_inc$t
+
+  # Number of censored.
+  s <- agegroups.info$s_k
+
+  # Number of cases.
+  r <- agegroups.info$r_k
+
+  # Age group label.
+  k <- agegroups.info$k
+
+  # Obtain incidence rate based on sample data.
   n_agegroups <- nrow(agegroups.info)
   for (k in 1:n_agegroups){
-    if(k!=n_agegroups){calcsum <- sum(agegroups.info$r_k[(k+1):n_agegroups], agegroups.info$s_k[(k+1):n_agegroups])} else {calcsum <- 0}
-    agegroups.info$mu_k[k] = agegroups.info$r_k[k]/ (agegroups.info$p_k[k]+agegroups.info$q_k[k]+agegroups.info$t[k]*calcsum)
+    if(k!=n_agegroups){calcsum <- sum(agegroups.info$r_k[(k+1):n_agegroups],
+                                      agegroups.info$s_k[(k+1):n_agegroups])
+    } else {calcsum <- 0}
+    agegroups.info$mu_k[k] = agegroups.info$r_k[k]/
+      (agegroups.info$p_k[k]+agegroups.info$q_k[k]+agegroups.info$t[k]*calcsum)
 
   }
 
-  # Recalculate S_k. (in the sample!) based on the intervals
+  # Recalculate S_k. (in the sample!) based on the intervals.
   Surv_k. <- c(1, exp(-cumsum(agegroups.info$mu_k*t)))
   S_k. <- c() # Initialize vector.
   for (i in 1:length(Surv_k.)){
@@ -133,7 +176,8 @@ Prepare_data <- function(dat, population_incidence, breaks){
   agegroups.info$S_k. <- S_k.
   agegroups.info$S_k <- dat_inc$S_k
 
-  # Add S_k. to the sample
+  # --------------- Add S_k. to the sample data.
+
   dat_out <- merge(dat, agegroups.info, by.x = "y_cat", by.y = "k") %>%
     mutate(k = y_cat) %>%
     mutate(population_incidence = mu_k)
@@ -142,7 +186,8 @@ Prepare_data <- function(dat, population_incidence, breaks){
   dat_out$y_cat <- NULL; dat_out$mu_k <- NULL;
   dat_out$s_k <- dat_out$r_k <- dat_out$p_k <- dat_out$q_k <- NULL
 
-  # Output the data.frame:
+  # --------------- Output the data.frame.
+
   dat_out
 
 }
